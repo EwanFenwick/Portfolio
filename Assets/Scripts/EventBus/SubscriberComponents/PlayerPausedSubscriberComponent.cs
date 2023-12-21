e9fe0f5f6049250d534f16e297dbbfe2fa4129dd
Ewan Fenwick <racfairfax@gmail.com>
2023-12-21 18:47:34 +0100

    tweaked PlayerPausedSubscriberComponent to allow more generic implementations

--- Assets/Scripts/EventBus/SubscriberComponents/PlayerPausedSubscriberComponent.cs
+++ Assets/Scripts/EventBus/SubscriberComponents/PlayerPausedSubscriberComponent.cs
@@ -4,10 +4,12 @@ using UnityEngine.Events;
 
 namespace Portfolio.EventBusSystem {
     public class PlayerPausedSubscriberComponent : EventBusSubscriberComponent<PausePlayerEvent> {
+        [SerializeField] private bool _invertBool;
         [SerializeField] private UnityEvent<bool> _uniEvent;
 
         protected override void OnEvent(object sender, EventArgs eventArgs) {
-            _uniEvent?.Invoke(!((PausePlayerEvent)eventArgs).IsPaused);
+            var pause = ((PausePlayerEvent)eventArgs).IsPaused;
+            _uniEvent?.Invoke(_invertBool ? !pause : pause);
         }
     }
 
