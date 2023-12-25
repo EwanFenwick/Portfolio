834b5ad314b6b13b4e0aa93b03fb8d5b0e6b75c3
Ewan Fenwick <racfairfax@gmail.com>
2023-12-25 02:23:10 +0100

    added choices to dialogue popup

--- Assets/Scripts/Popups/DialoguePopup/DialogueChoiceView.cs
+++ Assets/Scripts/Popups/DialoguePopup/DialogueChoiceView.cs
@@ -1,12 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Portfolio.Popups {
-    public class DialogueChoiceView : MonoBehaviour {
+    public class DialogueChoiceView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
         [SerializeField] private TextMeshProUGUI _choiceText;
+        [SerializeField] private Button _choiceButton;
 
-        public DialogueChoiceView(string choiceText) {
+        public Button ChoiceButton => _choiceButton;
+
+        public GameObject Indicator { get; set; }
+
+
+        public void Initialise(string choiceText, GameObject indicator) {
             _choiceText.text = choiceText;
+            Indicator = indicator;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData) {
+
+            Indicator.SetActive(true);
+            var indicatorPos = Indicator.transform.position;
+            Indicator.transform.position = new(transform.position.x, indicatorPos.y, indicatorPos.z);
+        }
+
+        public void OnPointerExit(PointerEventData eventData) {
+            Indicator.SetActive(false);
         }
     }
 }
